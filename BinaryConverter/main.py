516263e9dcb8a676e6829cb93dae75df249e446c
MAT06mat Desktop <mat06mat22@gmail.com>
2024-11-26 18:56:09 +0100

    Update

--- BinaryConverter/main.py
+++ BinaryConverter/main.py
@@ -3,8 +3,8 @@
 #
 
 while True:
+    nb = input("Choisissez un nombre (0x pour hex et 0b pour bin) : ")
     try:
-        nb = input("Choisissez un nombre (0x pour hex et 0b pour bin) : ")
         print("Binaire :", bin(int(nb)))
         print("Hexadécimal :", hex(int(nb)))
         print("Décimal :", int(nb))
